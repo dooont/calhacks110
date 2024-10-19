f9425f5a662395abe61b0970f219bd2b82b90653
dooont <matthewmaung@QQs-Laptop>
2024-10-19 12:28:22 -0700

    map div shows up

--- calhacks110/calhacks110.py
+++ calhacks110/calhacks110.py
@@ -196,9 +196,91 @@ TOILET MAP PAGE
 ----------------------------------------------------------------------------------------------------------------------
 """
 
-class MapComponent(rx.Component):
-    def render_toilets(self, **props):
-        return rx.box(
+# class MapComponent(rx.Component):
+#     def render_toilets(**props):
+#         return rx.box(
+#             rx.el.div(
+#                 id='map',
+#                 style={
+#                     'width': '100%',
+#                     'height': '100vh',
+#                 }
+#             ),
+#             rx.el.script(src='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.js'),
+#             rx.el.link(
+#                 href='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css',
+#                 rel='stylesheet'
+#             ),
+#             rx.el.script(
+#                 f"""
+#                 mapboxgl.accessToken = 'pk.eyJ1IjoiZnJhbmtjaGFuZzEwMDAiLCJhIjoiY20xbGFzcG1hMDNvaTJxbjY3a3N4NWw4dyJ9.W78DlIwDnlVOrCE5F1OnkQ';
+#                 const map = new mapboxgl.Map({{
+#                     container: 'map',
+#                     style: 'mapbox://styles/mapbox/streets-v11',
+#                     center: [-122.4194, 37.7749], // Centered on San Francisco
+#                     zoom: 12
+#                 }});
+
+#                 const toilets = {props.get('toilets')};
+
+#                 toilets.forEach(function(toilet) {{
+#                     new mapboxgl.Marker()
+#                         .setLngLat([toilet.longitude, toilet.latitude])
+#                         .setPopup(new mapboxgl.Popup().setHTML(
+#                             `<h3>${{toilet.name}}</h3>
+#                             <p>Type: ${{toilet.resource_type}}</p>
+#                             <p>Access: ${{toilet.access}}</p>`
+#                         ))
+#                         .addTo(map);
+#                 }});
+#                 """
+#             ),
+#         )
+# class MapComponent(rx.Component):
+#     def render_toilets(**props):
+#         return rx.box(
+#             rx.el.div(
+#                 id='map',
+#                 style={
+#                     'width': '100%',
+#                     'height': '100vh',
+#                 }
+#             ),
+#             rx.el.script(src='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.js'),
+#             rx.el.link(
+#                 href='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css',
+#                 rel='stylesheet'
+#             ),
+#             rx.el.script(
+#                 f"""
+#                 mapboxgl.accessToken = 'pk.eyJ1IjoiZnJhbmtjaGFuZzEwMDAiLCJhIjoiY20xbGFzcG1hMDNvaTJxbjY3a3N4NWw4dyJ9.W78DlIwDnlVOrCE5F1OnkQ';
+#                 const map = new mapboxgl.Map({{
+#                     container: 'map',
+#                     style: 'mapbox://styles/mapbox/streets-v11',
+#                     center: [-122.4194, 37.7749], // Centered on San Francisco
+#                     zoom: 12
+#                 }});
+
+#                 const toilets = {props.get('toilets', '[]')};  // Fallback to empty array if no toilets are passed
+
+#                 toilets.forEach(function(toilet) {{
+#                     new mapboxgl.Marker()
+#                         .setLngLat([toilet.longitude, toilet.latitude])
+#                         .setPopup(new mapboxgl.Popup().setHTML(
+#                             `<h3>${{toilet.name}}</h3>
+#                             <p>Type: ${{toilet.resource_type}}</p>
+#                             <p>Access: ${{toilet.access}}</p>`
+#                         ))
+#                         .addTo(map);
+#                 }});
+#                 """
+#             ),
+#         )
+
+
+def render_toilets(**props):
+    return rx.box(
+            rx.text("This is a test to see if the component container is working"),
             rx.el.div(
                 id='map',
                 style={
@@ -213,16 +295,15 @@ class MapComponent(rx.Component):
             ),
             rx.el.script(
                 f"""
-                mapboxgl.accessToken = 'pk.eyJ1IjoiZnJhbmtjaGFuZzEwMDAiLCJhIjoiY20xbGFzcG1hMDNvaTJxbjY3a3N4NWw4dyJ9.W78DlIwDnlVOrCE5F1OnkQ';
+                console.log({props.get('toilets')});  // Log the toilets data to the console
+                mapboxgl.accessToken = 'your-access-token-here';
                 const map = new mapboxgl.Map({{
                     container: 'map',
                     style: 'mapbox://styles/mapbox/streets-v11',
-                    center: [-122.4194, 37.7749], // Centered on San Francisco
+                    center: [-122.4194, 37.7749],  // Centered on San Francisco
                     zoom: 12
                 }});
-
-                const toilets = {props.get('toilets')};
-
+                const toilets = {props.get('toilets', '[]')};  // Fallback to empty array if no toilets are passed
                 toilets.forEach(function(toilet) {{
                     new mapboxgl.Marker()
                         .setLngLat([toilet.longitude, toilet.latitude])
@@ -236,6 +317,33 @@ class MapComponent(rx.Component):
                 """
             ),
         )
+
+
+def load_toilet_data():
+    return [
+        {"name": "Public Toilet 1", "longitude": -122.4194, "latitude": 37.7749, "resource_type": "Public", "access": "Free"},
+        {"name": "Public Toilet 2", "longitude": -122.4294, "latitude": 37.7849, "resource_type": "Public", "access": "Free"},
+        # Add more toilet data here
+    ]
+
+# def load_toilet_data():
+#     toilets = []
+#     with open('frontend/assets/clean_data.csv', mode='r', encoding='utf-8-sig') as csvfile:
+#         reader = csv.DictReader(csvfile)
+#         for row in reader:
+#             toilets.append({
+#                 'name': row['name'],
+#                 'resource_type': row['resource_type'],
+#                 'latitude': float(row['latitude']),
+#                 'longitude': float(row['longitude']),
+#                 'access': row['access'],
+#                 'public_access_hours_open': row['public_access_hours_open'],
+#                 'public_access_hours_close': row['public_access_hours_close'],
+#                 'public_access_days': row['public_access_days'],
+#                 'data_as_of': row['data_as_of'],
+#                 # Add other fields as needed
+#             })
+#     return toilets
 
 app = rx.App()
 
@@ -262,10 +370,9 @@ def index() -> rx.Component:
 @rx.page(route="/dashboard", title="Dashboard")
 def dashboard() -> rx.Component:
     return rx.container(
+        rx.text("This is a test to see if the container is working"),
         rx.color_mode.button(position="top-right"),
-        MapComponent.create(), #toilets=load_toilet_data
-        # width="100vw",
-        # background_color="white",
+        render_toilets(toilets=load_toilet_data()), #toilets=load_toilet_data
     )
 
 @rx.page(route="/signup", title="Sign Up")
--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,4 @@ import reflex as rx
 
 config = rx.Config(
     app_name="calhacks110",  # Correct the module path
-    # app_name="frontend.pages.Main",  # Update to point to main.py
 )
